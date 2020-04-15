d2c7e76ee36bf74c7d0031ae5371826fd88dc636
uenoryo <uenoryo@uma-b.com>
2020-04-15 21:15:33 +0900

    mapで返すようにする.

--- gcpenv/gcpenv.go
+++ gcpenv/gcpenv.go
@@ -2,7 +2,8 @@ package gcpenv
 
 import (
 	"context"
-	"fmt"
+	"io"
+	"sync"
 
 	"github.com/pkg/errors"
 	"github.com/uenoryo/gcp-env/gcloud/secretmanager"
@@ -12,11 +13,15 @@ import (
 // GOOGLE_APPLICATION_CREDENTIALS に認証情報が必要です
 type GCPEnv struct {
 	config *Config
+	values *sync.Map
 }
 
 // New (､´･ω･)▄︻┻┳═一
 func New(conf *Config) *GCPEnv {
-	return &GCPEnv{config: conf}
+	return &GCPEnv{
+		config: conf,
+		values: &sync.Map{},
+	}
 }
 
 // Fetch は環境変数を取得する
@@ -38,15 +43,34 @@ func (env *GCPEnv) Fetch(ctx context.Context) error {
 		req := &secretmanager.AccessSecretVersionRequest{
 			ProjectName: "chitoi",
 			Key:         key,
-			//			Version:     "1",
+			Version:     "1",
 		}
 		res, err := client.AccessSecretVersion(ctx, req)
 		if err != nil {
 			return errors.Wrapf(err, "failed to access secret. key:[%s]", key)
 		}
-		fmt.Printf("%s=%s\n", key, res.Value)
+		env.values.Store(key, res.Value)
 	}
 	return nil
+}
+
+// Write は環境変数を w に書き込む
+func (env *GCPEnv) Write(w io.Writer) error {
+	return nil
+}
+
+// Map は取得したデータをmapで返す
+func (env *GCPEnv) Map() map[string]string {
+	m := map[string]string{}
+	env.values.Range(func(key, value interface{}) bool {
+		k, kOk := key.(string)
+		v, vOk := value.(string)
+		if kOk && vOk {
+			m[k] = v
+		}
+		return true
+	})
+	return m
 }
 
 // Config (､´･ω･)▄︻┻┳═一
--- main.go
+++ main.go
@@ -12,4 +12,5 @@ func main() {
 	if err := env.Fetch(context.Background()); err != nil {
 		log.Println(err.Error())
 	}
+	log.Println(env.Map())
 }
